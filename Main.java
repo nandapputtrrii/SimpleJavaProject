d39faa3c382eb3ec1296f2b2f45709fb1b9f96ba
nandappuuttrrii <nandappuuttrrii12@gmail.com>
2025-10-19 21:22:35 +0700

    Add second output line

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Git!");
+        System.out.println("Hello, Advanced Programming!");
         System.out.println("This is my first Java project with Git.");
     }
 }
